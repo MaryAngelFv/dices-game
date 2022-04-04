1b2cbb55451cb8a714992eed7241ba969afc3426
MaryAngelFv <90918206+MaryAngelFv@users.noreply.github.com>
2022-04-03 19:03:58 -0500

    Initial Commit

---
+++ 02-dices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02-dices
+//
+//  Created by Brayam Mora on 3/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
